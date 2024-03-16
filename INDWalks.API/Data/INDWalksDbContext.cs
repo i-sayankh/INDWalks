be3b2a61ceda576bd36f767416c6dffa03ba29c4
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-03-16 11:43:14 +0530

    Creating the DbContext Class

---
+++ INDWalks.API/Data/INDWalksDbContext.cs
@@ -0,0 +1,13 @@
+﻿using INDWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace INDWalks.API.Data
+{
+    public class INDWalksDbContext : DbContext
+    {
+        public INDWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walks> Walks { get; set; }
+    }
+}
